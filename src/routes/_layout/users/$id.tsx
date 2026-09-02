b44e2b0492dd58082596e0bc378ff6c926c9f503
Andres Court <andres@andrescourt.com>
2026-09-02 17:24:59 -0500

    feat(dyn): validate params

--- src/routes/_layout/users/$id.tsx
+++ src/routes/_layout/users/$id.tsx
@@ -1,6 +1,15 @@
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_layout/users/$id')({
+	params: {
+		parse: (rawParams) => {
+			const id = Number.parseInt(rawParams.id, 10)
+			if (Number.isNaN(id)) {
+				throw new Error('Invalid id')
+			}
+			return { id }
+		}
+	},
 	component: RouteComponent,
 })
 
--- src/routes/_layout/users/index.tsx
+++ src/routes/_layout/users/index.tsx
@@ -1,9 +1,20 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_layout/users/')({
 	component: RouteComponent,
 })
 
 function RouteComponent() {
-	return <div>Hello "/users/"!</div>
+	return (
+		<div>
+			<h1>Users</h1>
+			<ul>
+				<li>
+					<Link to='/users/$id' params={{ id: 44 }}>
+						User 1
+					</Link>
+				</li>
+			</ul>
+		</div>
+	)
 }
