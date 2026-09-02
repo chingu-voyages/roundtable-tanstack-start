6c87dc2192f9b5f56dcbc22d19cc082e3325ca6b
Andres Court <andres@andrescourt.com>
2026-09-02 17:49:43 -0500

    added hooks

---
+++ src/components/form/formButton.tsx
@@ -0,0 +1,19 @@
+import { ComponentProps } from 'react'
+import { Button } from '../ui/button'
+import { useFormContext } from '@/hooks/form-context'
+
+interface FormButtonProps extends ComponentProps<typeof Button> {
+	label?: string
+}
+
+export function FormButton({ label = 'Submit', ...props }: FormButtonProps) {
+	const form = useFormContext()
+
+	return (
+		<form.Subscribe selector={(state) => state.isSubmitting}>
+			<Button type='submit' variant='default' {...props}>
+				{label}
+			</Button>
+		</form.Subscribe>
+	)
+}
